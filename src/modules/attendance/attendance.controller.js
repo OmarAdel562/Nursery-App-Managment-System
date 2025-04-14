e0f31d1f40ca3e1c78458a8deeb46c5c74b3959f
OmarAdel562 <OmarAdel562>
2025-04-14 06:58:08 +0200

    delete api

--- src/modules/attendance/attendance.controller.js
+++ src/modules/attendance/attendance.controller.js
@@ -61,32 +61,3 @@ export const leaveAttendance = async (req, res) => {
          success:true,
          data:createdleave})
  }
-//  // ---------------------1-تسجيل الحضور---------------------------------
-// export const markAttendance = async (req, res) => {
-//   const response = await axios.get('http://localhost:5001/api/face-recognition');
-//   const recognizedId = response.data.recognized_name
-
-//   if (recognizedId === "Unknown") {
-//     return next( new AppErorr(message.student.notFound,404))
-//   }
-//    // check student existance
-//    const student = await Student.findOne({ userId: recognizedId }); 
-//    if (!student) {
-//     return next( new AppErorr(message.student.notFound,404))
-//    }
-//   const attendance = new Attendance({
-//     studentId: recognizedId,
-//     date: new Date(),
-//     status: 'present',
-//   })
-
-//   const createdattendance = await attendance.save()
-//   if(!createdattendance){
-//           return next( new AppErorr(message.attendance.fileToCreate,500))
-//        }
-
-//     return res.status(201).json({message:message.attendance.createsuccessfully,
-//          success:true,
-//          data:createdattendance})
- 
-//  }
