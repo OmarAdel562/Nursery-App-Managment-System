5ab9dbe1aa698f802d9bae85a90ec89a971f6f58
OmarAdel562 <adel35409@gmail.com>
2025-04-27 19:05:15 +0300

    first commit

--- src/modules/pronunciation/pronunciation.controller.js
+++ src/modules/pronunciation/pronunciation.controller.js
@@ -1,22 +1,3 @@
-// import axios from 'axios'
-//
-// export const analyzePronunciation = async (req, res, next) => {
-//   const { audio_url, word } = req.body
-//
-//   if (!audio_url || !word) {
-//     return res.status(400).json({ status: 'error', message: 'audio_url and word are required' })
-//   }
-//
-//   const response = await axios.post('http://localhost:5000/analyze', {
-//     audio_url,
-//     word
-//   })
-//
-//   return res.status(200).json({
-//     status: 'success',
-//     result: response.data.result,
-//   })
-// }
 import axios from 'axios';
 import cloudinary from 'cloudinary';
 
@@ -27,7 +8,6 @@ export const analyzePronunciation = async (req, res, next) => {
   if (!audioFile || !word) {
     return res.status(400).json({ status: 'error', message: 'An audio file and a word are required' });
   }
-
   try {
     // Upload the audio file to Cloudinary
     const uploadResult = await cloudinary.uploader.upload(audioFile.tempFilePath, {
