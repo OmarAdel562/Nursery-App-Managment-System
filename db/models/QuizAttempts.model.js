2898a9b10c13fedbd83321f62ec6cfe82be4bbab
OmarAdel562 <adel35409@gmail.com>
2025-05-03 19:16:29 +0300

    first commit

--- db/models/QuizAttempts.model.js
+++ db/models/QuizAttempts.model.js
@@ -3,7 +3,7 @@ import { model, Schema } from "mongoose";
 //schema
 const quizattemptsSchema= new Schema(
     {
-        studentId: { type:Schema.Types.ObjectId,ref:"Student",  required:true },
+        studentId: { type:Schema.Types.ObjectId,ref:"User",  required:true },
         quizId: {  type:Schema.Types.ObjectId, ref: "Quiz", required: true },
         score: { type: Number, default: 0 }, 
         startedAt: { type: Date },
