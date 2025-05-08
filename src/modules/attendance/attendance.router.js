27ea22da59213dc3c86c535a3d35133bcfb52e40
OmarAdel562 <adel35409@gmail.com>
2025-05-08 05:08:45 +0300

    first commit

--- src/modules/attendance/attendance.router.js
+++ src/modules/attendance/attendance.router.js
@@ -1,8 +1,5 @@
 import { Router } from "express";
 import { asyncHandler } from "../../utils/AppError.js";
-import { isAuthenticated } from "../../midderware/authentication.js";
-import { isAuthorized } from "../../midderware/authorization.js";
-import { roles } from "../../utils/constant/enum.js";
 import { upload, markAttendance, leaveAttendance } from "./attendance.controller.js";
 
 const attendanceRouter = Router();
@@ -12,8 +9,6 @@ const attendanceRouter = Router();
 // 1- Mark Attendance (with file upload)
 attendanceRouter.post(
     "/createattendance",
-    isAuthenticated, // Ensure the user is authenticated
-    isAuthorized(roles.STUDENT), // Ensure the user has the correct role
     upload.single("uploadedImage"), // Use multer to handle single file upload
     asyncHandler(markAttendance) // Call the controller function
 );
@@ -21,8 +16,6 @@ attendanceRouter.post(
 // 2- Leave Attendance (with file upload)
 attendanceRouter.post(
     "/leaveattendance",
-    isAuthenticated, // Ensure the user is authenticated
-    isAuthorized(roles.STUDENT), // Ensure the user has the correct role
     upload.single("uploadedImage"), // Use multer to handle single file upload
     asyncHandler(leaveAttendance) // Call the controller function
 );
